6b2ff411d55f6340e0075dec1c78938fb3b65671
MrTrotid <bamanguragain@gmail.com>
2026-05-03 18:22:50 +0545

    feat: finalize portfolio with proper meta description and llms.txt

--- app/layout.tsx
+++ app/layout.tsx
@@ -39,7 +39,7 @@ export const metadata: Metadata = {
     template: '%s | Baman Prasad Guragain (MrTrotid)',
   },
   description:
-    'Hey there this is my portfolio where you will find most of the things about me.',
+    'Hey there! This is my portfolio where you will find most of the things about me - a cybersecurity enthusiast and creative developer from Nepal.',
   category: 'technology',
   applicationName: "Baman Prasad Guragain Portfolio",
   keywords: [
